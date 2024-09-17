13c9eb1b834378366c139d70650d8a01dde2440c
beau currier <beaucurrier01@gmail.com>
2024-09-17 01:05:59 -0700

    added props and the date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
+
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+    
+      const [date, setDate] = useState(new Date());
+    
+      useEffect(() => {
+        const intervalId = setInterval(() => {
+          setDate(new Date());
+        }, 1000);
+        return () => clearInterval(intervalId);
+      }, []);
+    
+      const formattedDate = date.toLocaleDateString();
+    
+      return (
+        <div>
+          <h3>Today's date: {formattedDate}</h3>
+        </div>
+      );
+    
 }
 
 export default Footer
