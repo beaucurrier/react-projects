de2154b37e14f6d9d3b51b151fd99912a642d23b
beau currier <beaucurrier01@gmail.com>
2024-10-03 18:53:56 -0700

    vite.config change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      '/todos': {
+        target: 'https://todolistbackend-fb6e.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
