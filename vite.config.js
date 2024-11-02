5e01bca00138a3b394e488b3f16a2ab308181bc0
beau currier <beaucurrier01@gmail.com>
2024-11-01 19:01:13 -0700

    updating the todo app

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/todos': 'https://todolistbackend-fb6e.onrender.com',
-//       },
-//     },
-//   plugins: [react()],
-// })
-
 export default defineConfig({
+  server: {
+    proxy: {
+      '/todos': 'https://todolistbackend-fb6e.onrender.com',
+      '/add-item': 'https://todolistbackend-fb6e.onrender.com',
+      '/delete-item': 'https://todolistbackend-fb6e.onrender.com',
+      '/update-item': 'https://todolistbackend-fb6e.onrender.com',
+      },
+    },
   plugins: [react()],
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
